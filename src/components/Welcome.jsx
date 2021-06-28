c854887319f89536f385ff65632af08403c0b825
Jano Bokuchava <https://github.com/login/oauth/authorize?state=socketId%3A50e1b855-ef2e-41fe-ac81-bf197b50c84e%3Baction%3Alogin%3BinApp%3Atrue%3BneedsGKPermissions%3Atrue%3Bchallenge%3A0f7cfbf0-9c29-49b8-8d88-7d83fb0a3cb9%3B&scope=admin%3Apublic_key%2Crepo%2Cworkflow%2Cread%3Aorg%2Cuser%3Aemail&response_type=code&client_id=a7557949433b7d282a76>
2021-06-28 21:29:05 +0400

    react 01

---
+++ src/components/Welcome.jsx
@@ -0,0 +1,9 @@
+function Welcome(props) {
+  return (
+    <div className="row shadow p-2">
+      <h2 className="text-danger">{props.title}</h2>
+    </div>
+  );
+}
+
+export default Welcome;
---
+++ src/utils/helpers.js
@@ -0,0 +1,9 @@
+export const getTen = () => 10;
+export const addTen = (n) => n + 10;
+export const sum = (a, b) => a + b;
+export const subtract = (a, b) => a - b;
+
+// snake_case
+// kebab-case
+// PascalCase
+// camelCase
